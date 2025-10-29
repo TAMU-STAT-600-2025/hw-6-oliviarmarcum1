40e75d8a488b929a9a4d76a2953de629a100d2e6
oliviarmarcum1 <oliviarmarcum@tamu.edu>
2025-10-29 11:31:17 -0500

    test for soft-thresholding function and test for lasso objective function

--- TestsCpp.R
+++ TestsCpp.R
@@ -11,11 +11,35 @@ source("LassoFunctions.R")
 
 # Do at least 2 tests for soft-thresholding function below. You are checking output agreements on at least 2 separate inputs
 #################################################
-
+set.seed(1)
+stopifnot(all.equal(soft_c(3.0, 0.5), soft(3.0, 0.5)))
+stopifnot(all.equal(soft_c(-0.12, 0.2), soft(-0.12, 0.2)))
+# (|a| < lambda and |a| > lambda)
+stopifnot(all.equal(soft_c(0.05, 0.2), soft(0.05, 0.2)))
+stopifnot(all.equal(soft_c(-2.3, 0.1), soft(-2.3, 0.1)))
 
 # Do at least 2 tests for lasso objective function below. You are checking output agreements on at least 2 separate inputs
 #################################################
+n <- 120; p <- 50
+Xraw <- matrix(rnorm(n * p), n, p)
+beta_true <- c(rep(2, 5), rep(0, p - 5))
+Yraw <- as.numeric(Xraw %*% beta_true + rnorm(n))
 
+std <- standardizeXY(Xraw, Yraw)
+Xtilde <- std$Xtilde
+Ytilde <- std$Ytilde
+
+lambda1 <- 0.15
+lambda2 <- 0.6
+b_try1 <- rnorm(p)
+b_try2 <- rnorm(p)
+
+stopifnot(all.equal(lasso_c(Xtilde, Ytilde, b_try1, lambda1),
+                    lasso(Xtilde, Ytilde, b_try1, lambda1),
+                    tolerance = 1e-10))
+stopifnot(all.equal(lasso_c(Xtilde, Ytilde, b_try2, lambda2),
+                    lasso(Xtilde, Ytilde, b_try2, lambda2),
+                    tolerance = 1e-10))
 
 # Do at least 2 tests for fitLASSOstandardized function below. You are checking output agreements on at least 2 separate inputs
 #################################################
